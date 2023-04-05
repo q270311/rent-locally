c865e273e36804ebde4ecf65aaf70bc3fa3afb71
Michał Bałdyga <q270311@gmail.com>
2023-04-05 14:08:49 +0200

    add colors to theme

--- src/core/theme.js
+++ src/core/theme.js
@@ -1,12 +1,17 @@
 export const theme = {
   color: {
     white: "#FFFFFF",
-    grey: "#F9F8F3",
+    orange: "#FDB137",
+    whiteOrange: "#FEEAA5",
+    lightGrey: "#F9F8F3",
+    grey: "#C1D1D1",
+    cuttySark: "#4A686A",
+    black: "#000000",
   },
 
   breakpoint: {
-    // tabletDevice: 992,
+    //tabletDevice: 992,
     mobileDevice: 767,
-    // smallDevice: 550,
+    smallDevice: 550,
   },
 };
