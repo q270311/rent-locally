08bdae02bed846a1a64ceded1b08f09ef6f71d98
Michał Bałdyga <q270311@gmail.com>
2023-04-12 14:50:42 +0200

    add QueryClientProvider to App

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './core/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./core/App";
+import reportWebVitals from "./reportWebVitals";
 import { GlobalStyle } from "./core/GlobalStyle";
 import { theme } from "./core/theme";
 import { ThemeProvider } from "styled-components";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient();
+
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <ThemeProvider theme={theme}>
-        <GlobalStyle />
-          <App />
-      </ThemeProvider>
-  </React.StrictMode>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
