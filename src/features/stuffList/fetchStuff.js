8b8888fc98a692f7b58db9abb89acea4902e4177
Michał Bałdyga <q270311@gmail.com>
2023-04-13 12:11:53 +0200

    remove fetchStuff function to separate file

---
+++ src/features/stuffList/fetchStuff.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const fetchStuff = async () =>
+  await axios
+    .get("https://sharestuff.onrender.com/api/stuff")
+    .then((response) => response.data);
+
+export default fetchStuff;
