02e6784795950f9b983433e292c979280eb7b7a2
Michał Bałdyga <q270311@gmail.com>
2023-04-13 14:16:36 +0200

    rename folder

--- src/core/App.js
+++ src/core/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { toStuffList } from "./routes";
-import StuffList from "../features/stuffList";
+import StuffList from "../features/StuffList";
 
 function App() {
   return (
