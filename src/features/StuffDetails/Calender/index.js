0e319dfc926fa021c093e9c954c2ceaf35df1f05
Michał Bałdyga <q270311@gmail.com>
2023-04-17 14:10:33 +0200

    Calender component added

--- src/features/StuffDetails/Calender/index.js
+++ src/features/StuffDetails/Calender/index.js
@@ -2,15 +2,40 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchReservations from "./fetchReservations";
-
-import { Wrapper } from "./styled";
+import { nanoid } from "@reduxjs/toolkit";
+import { Wrapper, Ul, Li, Label } from "./styled";
 
 const Content = ({ content }) => (
   <div>
-    <h1>Calender</h1>
+    <h1>Lista dostępności</h1>
     <Wrapper> {content} </Wrapper>
   </div>
 );
+
+const ReservationCheckbox = ({ reservation, setReservationHandler }) => {
+  const [isChecked, setChecked] = useState(false);
+
+  return (
+    <Label 
+      disabled={reservation.reserved === 1}
+      checked={reservation.reserved === 1 ? true : isChecked}
+      >
+      <input
+        type="checkbox"
+        disabled={reservation.reserved === 1}
+        checked={reservation.reserved === 1 ? true : isChecked}
+        onChange={({ target }) => {
+          if (reservation.reserved === 0) {
+            setChecked(target.checked);
+            setReservationHandler(reservation.date.substring(0, 10));
+          }
+        }}
+      />
+      {reservation.date.substring(0, 10)}{" "}
+      {reservation.reserved === 1 ? " - zajęty" : isChecked ? "Zarezerwuj" : ""}
+    </Label>
+  );
+};
 
 const Calender = () => {
   const { id } = useParams();
@@ -18,6 +43,18 @@ const Calender = () => {
     fetchReservations({ stuffId: id })
   );
   const [content, setContent] = useState(null);
+  const [selectedReservation, setReservation] = useState([]);
+
+  const setReservationHandler = (date) => {
+    const index = selectedReservation.findIndex(
+      (reservation) => reservation === date
+    );
+    if (index === -1) {
+      setReservation(selectedReservation.push(date));
+    } else {
+      setReservation(selectedReservation.splice(index, 1));
+    }
+  };
 
   useEffect(() => {
     if (isLoading) {
@@ -26,13 +63,13 @@ const Calender = () => {
       setContent(`Mamy błąd... ${error.message}`);
     } else if (data) {
       setContent(
-        <ul>
-          {data.map((reservation) => (
-            <li>
-              {(reservation.date).substring(0, 10)} {(reservation.reserved===1? ' - zajęty' : '')}
-            </li>
-          ))}
-        </ul>
+        data.map((reservation) => (
+          <ReservationCheckbox
+            key={nanoid()}
+            reservation={reservation}
+            setReservationHandler={setReservationHandler}
+          />
+        ))
       );
     }
   }, [isLoading, error, data, id]);
--- src/features/StuffDetails/Calender/styled.js
+++ src/features/StuffDetails/Calender/styled.js
@@ -1,10 +1,12 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Wrapper = styled.main`
+export const Wrapper = styled.div`
+  padding-left: 20px;
   display: flex;
   flex-direction: column;
-  //display: grid;
-  //grid-template-columns: repeat(7, 1fr);
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  grid-gap: 5px;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileDevice}px) {
     display: flex;
@@ -13,4 +15,19 @@ export const Wrapper = styled.main`
     align-items: center;
     margin: 90px auto;
   }
+`;
+
+export const Label = styled.label`
+  ${({ checked }) =>
+    checked &&
+    css`
+      border: 1px solid ${({ theme }) => theme.color.orange};
+    `}
+
+  ${({ disabled }) =>
+    disabled &&
+    css`
+      color ${({ theme }) => theme.color.grey};
+      border: 1px solid ${({ theme }) => theme.color.lightGrey};
+    `}
 `;
