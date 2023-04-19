ad26d98b9a9ff62d274c1bda1e32804c812ec824
Michał Bałdyga <q270311@gmail.com>
2023-04-19 20:54:57 +0200

    reservationSlice file created

---
+++ src/features/StuffDetails/reservationSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const reservationSlice = createSlice({
+  name: "reservation",
+  initialState: {
+    reservation: [],
+  },
+  reducers: {
+    addRemoveReservation: (state, { payload }) => {
+      const index = state.reservation.findIndex((date) => date === payload);
+      if (index === -1) {
+        state.reservation.push(payload);
+      } else {
+        state.reservation.splice(index, 1);
+      }
+    },
+  },
+  });
+  
+export const { addRemoveReservation } = reservationSlice.actions;
+
+export const selectReservationState = (state) => state.reservation;
+export const selectReservation = (state) =>
+  selectReservationState(state).reservation;
+
+export default reservationSlice.reducer;
