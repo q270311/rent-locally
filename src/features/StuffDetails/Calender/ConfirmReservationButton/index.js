bed9fd013d5e622a035fea10b4b0c6c49789c5cd
Michał Bałdyga <q270311@gmail.com>
2023-04-19 20:59:21 +0200

    ConfirmReservationButton component added

---
+++ src/features/StuffDetails/Calender/ConfirmReservationButton/index.js
@@ -0,0 +1,11 @@
+import { useSelector } from "react-redux";
+import { selectReservation } from "../../reservationSlice";
+import { Button } from "./styled";
+
+const ConfirmReservationButton = () => {
+  const res = useSelector(selectReservation);
+
+  return <Button onClick={() => console.log(res)}>Confirm reservation</Button>;
+};
+
+export default ConfirmReservationButton;
---
+++ src/features/StuffDetails/Calender/ConfirmReservationButton/styled.js
@@ -0,0 +1,11 @@
+import styled, { css } from "styled-components";
+
+export const Button = styled.button`
+  margin:20px;
+
+  ${({ isHidden }) =>
+    isHidden &&
+    css`
+      display: none;
+    `}
+`;
