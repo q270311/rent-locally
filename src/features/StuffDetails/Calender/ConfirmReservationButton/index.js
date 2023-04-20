fa9efb97ca2fa57a55f806a7d5b12f382cc493cf
Michał Bałdyga <q270311@gmail.com>
2023-04-20 21:26:20 +0200

    getDateRanges function added

--- src/features/StuffDetails/Calender/ConfirmReservationButton/index.js
+++ src/features/StuffDetails/Calender/ConfirmReservationButton/index.js
@@ -2,10 +2,33 @@ import { useSelector } from 'react-redux'
 import { selectReservation } from '../../reservationSlice'
 import { Button } from './styled'
 
-const ConfirmReservationButton = () => {
-  const res = useSelector(selectReservation)
+const getDateRanges = (reservationsTable) => {
+  const reservations=[...reservationsTable].sort()
+  let start = reservations[0]
+  let end = reservations[0]
+  for (let i = 1; i < reservations.length; i++) {
+    let date = reservations[i]
+    let prevDate = reservations[i - 1]
+    let prevDay = new Date(prevDate)
+    let currentDay = new Date(date)
+    prevDay.setDate(prevDay.getDate() + 1)
+    if (prevDay.getTime() !== currentDay.getTime()) {
+      console.log(start + ' - ' + end)
+      start = date
+    }
+    end = date
+  }
+  console.log(start + ' - ' + end)
+}
 
-  return <Button onClick={() => console.log(res)}>Confirm reservation</Button>
+const ConfirmReservationButton = () => {
+  const reservations = useSelector(selectReservation)
+
+  return (
+    <Button onClick={() => getDateRanges(reservations)}>
+      Confirm reservation
+    </Button>
+  )
 }
 
 export default ConfirmReservationButton
