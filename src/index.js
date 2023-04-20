b09ee088b0729c94bf20ac90ef0d2b252725cad3
Michał Bałdyga <q270311@gmail.com>
2023-04-20 11:19:51 +0200

    source code formatted

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./core/App";
-import reportWebVitals from "./reportWebVitals";
-import { GlobalStyle } from "./core/GlobalStyle";
-import { theme } from "./core/theme";
-import { ThemeProvider } from "styled-components";
-import { Provider } from "react-redux";
-import store from "./core/store";
-import { HashRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './core/App'
+import reportWebVitals from './reportWebVitals'
+import { GlobalStyle } from './core/GlobalStyle'
+import { theme } from './core/theme'
+import { ThemeProvider } from 'styled-components'
+import { Provider } from 'react-redux'
+import store from './core/store'
+import { HashRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const queryClient = new QueryClient();
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const queryClient = new QueryClient()
 
 root.render(
   <QueryClientProvider client={queryClient}>
@@ -24,9 +24,9 @@ root.render(
       </ThemeProvider>
     </Provider>
   </QueryClientProvider>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
