31c58ae82a35e17fb21926560fec07f6fbde4c67
Michał Bałdyga <q270311@gmail.com>
2023-04-24 22:08:57 +0200

    rangesReservations table to getDateRanges function

--- src/features/StuffDetails/Calender/ConfirmReservationButton/index.js
+++ src/features/StuffDetails/Calender/ConfirmReservationButton/index.js
@@ -2,23 +2,26 @@ import { useSelector } from 'react-redux'
 import { selectReservation } from '../../reservationSlice'
 import { Button } from './styled'
 
-const getDateRanges = (reservationsTable) => {
-  const reservations=[...reservationsTable].sort()
-  let start = reservations[0]
-  let end = reservations[0]
-  for (let i = 1; i < reservations.length; i++) {
-    let date = reservations[i]
-    let prevDate = reservations[i - 1]
+const getDateRanges = (reservations) => {
+  const sortedReservations = [...reservations].sort()
+  let rangesReservations = []
+  let start = sortedReservations[0]
+  let end = sortedReservations[0]
+  for (let i = 1; i < sortedReservations.length; i++) {
+    let date = sortedReservations[i]
+    let prevDate = sortedReservations[i - 1]
     let prevDay = new Date(prevDate)
     let currentDay = new Date(date)
     prevDay.setDate(prevDay.getDate() + 1)
     if (prevDay.getTime() !== currentDay.getTime()) {
+      rangesReservations = [...rangesReservations, { start, end }]
       console.log(start + ' - ' + end)
       start = date
     }
     end = date
   }
-  console.log(start + ' - ' + end)
+  rangesReservations = [...rangesReservations, { start, end }]
+  console.log(rangesReservations)
 }
 
 const ConfirmReservationButton = () => {
