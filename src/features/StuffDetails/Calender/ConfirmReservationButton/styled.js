e7a71fba6d0477bda95504f507f7b2a603f25a55
Michał Bałdyga <q270311@gmail.com>
2023-04-25 12:45:14 +0200

    add Wrapper to ConfirmReseervationButton

--- src/features/StuffDetails/Calender/ConfirmReservationButton/styled.js
+++ src/features/StuffDetails/Calender/ConfirmReservationButton/styled.js
@@ -1,5 +1,10 @@
 import styled, { css } from 'styled-components'
 
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`
+
 export const Button = styled.button`
   margin: 20px;
 
