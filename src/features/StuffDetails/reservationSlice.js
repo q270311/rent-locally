2a4f5b0077d26fcb496be25f30889eaf43c2e090
Michał Bałdyga <q270311@gmail.com>
2023-04-25 12:44:22 +0200

    add selectDateRanges selector

--- src/features/StuffDetails/reservationSlice.js
+++ src/features/StuffDetails/reservationSlice.js
@@ -3,28 +3,50 @@ import { createSlice } from '@reduxjs/toolkit'
 const reservationSlice = createSlice({
   name: 'reservation',
   initialState: {
-    reservation: [],
+    reservations: [],
   },
   reducers: {
-    addRemoveReservation: (state, { payload }) => {
-      const index = state.reservation.findIndex((date) => date === payload)
+    addDeleteReservation: (state, { payload }) => {
+      const index = state.reservations.findIndex((date) => date === payload)
       if (index === -1) {
-        state.reservation.push(payload)
+        state.reservations.push(payload)
       } else {
-        state.reservation.splice(index, 1)
+        state.reservations.splice(index, 1)
       }
     },
-    deleteReservation: (state) => {
-      state.reservation = []
+    deleteAllReservation: (state) => {
+      state.reservations = []
     },
   },
 })
 
-export const { addRemoveReservation, deleteReservation } =
+export const { addDeleteReservation, deleteAllReservation } =
   reservationSlice.actions
 
 export const selectReservationState = (state) => state.reservation
-export const selectReservation = (state) =>
-  selectReservationState(state).reservation
+export const selectReservations = (state) =>
+  selectReservationState(state).reservations
+export const selectDateRanges = (state) => {
+  const sortedReservations = [
+    ...selectReservationState(state).reservations,
+  ].sort()
+  let reservationsRanges = []
+  let start = sortedReservations[0]
+  let end = sortedReservations[0]
+  for (let i = 1; i < sortedReservations.length; i++) {
+    let date = sortedReservations[i]
+    let prevDate = sortedReservations[i - 1]
+    let prevDay = new Date(prevDate)
+    let currentDay = new Date(date)
+    prevDay.setDate(prevDay.getDate() + 1)
+    if (prevDay.getTime() !== currentDay.getTime()) {
+      reservationsRanges = [...reservationsRanges, { start, end }]
+      start = date
+    }
+    end = date
+  }
+  reservationsRanges = [...reservationsRanges, { start, end }]
 
+  return reservationsRanges
+}
 export default reservationSlice.reducer
