828e842781b607c39287750b0c41237f9fcb8c09
Michał Bałdyga <q270311@gmail.com>
2023-04-25 12:43:40 +0200

    moved getDateRanges function to reservationSlice

--- src/features/StuffDetails/Calender/ConfirmReservationButton/index.js
+++ src/features/StuffDetails/Calender/ConfirmReservationButton/index.js
@@ -1,36 +1,17 @@
 import { useSelector } from 'react-redux'
-import { selectReservation } from '../../reservationSlice'
-import { Button } from './styled'
-
-const getDateRanges = (reservations) => {
-  const sortedReservations = [...reservations].sort()
-  let rangesReservations = []
-  let start = sortedReservations[0]
-  let end = sortedReservations[0]
-  for (let i = 1; i < sortedReservations.length; i++) {
-    let date = sortedReservations[i]
-    let prevDate = sortedReservations[i - 1]
-    let prevDay = new Date(prevDate)
-    let currentDay = new Date(date)
-    prevDay.setDate(prevDay.getDate() + 1)
-    if (prevDay.getTime() !== currentDay.getTime()) {
-      rangesReservations = [...rangesReservations, { start, end }]
-      console.log(start + ' - ' + end)
-      start = date
-    }
-    end = date
-  }
-  rangesReservations = [...rangesReservations, { start, end }]
-  console.log(rangesReservations)
-}
+import { selectDateRanges } from '../../reservationSlice'
+import { Wrapper, Button } from './styled'
 
 const ConfirmReservationButton = () => {
-  const reservations = useSelector(selectReservation)
+  const reservations = useSelector(selectDateRanges)
 
   return (
-    <Button onClick={() => getDateRanges(reservations)}>
-      Confirm reservation
-    </Button>
+    <Wrapper>
+      <Button onClick={() => console.log(reservations)}>
+        Confirm reservation
+      </Button>
+      <p>info</p>
+    </Wrapper>
   )
 }
 
